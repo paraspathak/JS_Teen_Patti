b9442216bc19b0904a00dcb10712bbae7d1f4d11
Paras Pathak <paras.pathak@mavs.uta.edu>
2019-06-10 16:28:44 -0500

    Optimization

--- src/output.js
+++ src/output.js
@@ -12,28 +12,31 @@ class Output{
             var name_player = document.createElement('div');
             name_player.innerHTML=names[i].name;
             name_player.style.position = "absolute";
-            name_player.style.left = "75px";
-            name_player.style.top = "35px";
+            name_player.style.width="100%";
+            name_player.style.top = "35%";
+            name_player.style.textAlign="center";
             div.append(name_player); 
             div.id=('div'+i);
             div.style.border = 'solid';
             div.style.width = '200px';
             div.style.height = '100px';
             div.style.position = 'absolute';
+            div.style.textAlign="center";
+            div.style.backgroundColor="#d9d9d9";
 
-            if(i==0){
+            if(i===0){
                 div.style.top = "8px";
                 div.style.left = "16px";
             }
-            else if(i ==1){
+            else if(i ===1){
                 div.style.bottom = "8px";
                 div.style.right = "16px";
             }
-            else if(i==2){
+            else if(i===2){
                 div.style.bottom = "8px";
                 div.style.left = "16px";
             }
-            else if (i ==3){
+            else if (i ===3){
                 div.style.top = "8px";
                 div.style.right = "16px";
             }
@@ -41,22 +44,32 @@ class Output{
         }
     }
     //Distributes card animation
-    distribute_card(){
-        for(var i =0; i<this.number_of_player; i++){
-            var    parent_element = document.getElementById("player_screen_tl");
-            if(i==1)    parent_element = document.getElementById("player_screen_br");
-            else if(i==2)    parent_element = document.getElementById("player_screen_bl");
-            else if(i==3)    parent_element = document.getElementById("player_screen_tr");
-            for(var j =0; j<3; j++){
-                var new_card = document.createElement('td');
-                new_card.style.border = "solid";
-                new_card.style.height = '200px';
-                new_card.style.width = "145px";
-                new_card.innerHTML= "JS TEEN PATTI";
-                new_card.id = (toString(i)+toString(j));
-                parent_element.append(new_card);
-            }    
-        }        
+    async distribute_card(){
+       var tl = document.getElementById("player_screen_tl");
+       var tr = document.getElementById("player_screen_tr");
+       var br = document.getElementById("player_screen_br");
+       var bl = document.getElementById("player_screen_bl");
+       document.getElementById("only").style.visibility="hidden";
+       for(var j =0; j<3; j++){
+            if(this.number_of_player>0){
+                tl.append(create_card(0,j));
+                await sleep(500);                
+            }
+            if(this.number_of_player>1){
+                br.append(create_card(1,j));
+                await sleep(500);                
+            }
+            if(this.number_of_player>2){
+                bl.append(create_card(2,j));
+                await sleep(500);                
+            }
+            if(this.number_of_player>3){
+                tr.append(create_card(3,j));
+                await sleep(500);                
+            }                
+        }       
+        document.getElementById("only").style.visibility="visible";
+        return true; 
     }
     //Displays the card received by player
     show_card_to_player(card, number){
@@ -104,3 +117,20 @@ class Output{
         }
     }
 }
+
+
+function create_card(i,j){
+    var new_card = document.createElement('td');
+    new_card.style.border = "solid";
+    new_card.style.height = '200px';
+    new_card.style.width = "145px";
+    //new_card.innerHTML= "JS TEEN PATTI";
+    new_card.id = (toString(i)+toString(j));
+    new_card.className="card";
+    new_card.style.backgroundColor="aliceblue";
+    return new_card;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
